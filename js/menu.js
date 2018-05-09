fbb56c701953382c07c241fd473c236a7916947c
bianca <bianca@nic.br>
2018-05-09 16:37:59 -0300

    Skills screen

--- js/menu.js
+++ js/menu.js
@@ -3,4 +3,19 @@ $("document").ready(function(){
 		$(".nav-item").removeClass("border-red");
 		$(this).addClass("border-red");
 	});
+	$(".skillButton").click(function(){
+		type = $(this).data("type");
+		$(".skillButton").removeClass("selected");
+		$(this).addClass("selected");
+		$(".check-type").css({"display" : "block"});
+		if(type !== "tudo"){
+			$(".check-type").each(function(){
+				if($(this).data("tecnology") !== type){
+					$(this).css({"display" : "none"});
+				}
+			});
+		}else{
+			$(".check-type").css({"display" : "block"});
+		}
+	});
 });
